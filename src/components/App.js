714b35ba46c39806dc4c4c7127311ae043e117e9
Miko Peltokorpi <miko.peltokorpi@gmail.com>
2022-07-07 19:53:04 +0300

    Create navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import NavBar from './common/NavBar'
+import { Routes, Route } from 'react-router-dom'
+import Home from './homepage/Home'
+import Shop from './shop/Shop'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <NavBar/>
+      <div className='container'>
+        <Routes>
+          <Route path='/' element = {<Home/>}/>
+          <Route path='/shop' element = {<Shop/>}/>
+        </Routes>
+      </div> 
+    </>
   )
 }
 
--- src/components/common/NavBar.js
+++ src/components/common/NavBar.js
@@ -1,8 +1,44 @@
 import React from 'react'
+import { Link, NavLink } from "react-router-dom"
+import './styles/NavBar.css'
 
 const NavBar = () => {
+
+  let activeStyle = {
+    color: "black",
+  };
+
   return (
-    <div>NavBar</div>
+    <nav className='nav'>
+      <Link to='/' className='site-name'>Online shop</Link>
+      <ul>
+        <li>
+          <NavLink
+              to="/"
+              style={({ isActive }) =>
+              isActive ? activeStyle : undefined
+              }
+              >
+              Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+              to="/shop"
+              style={({ isActive }) =>
+              isActive ? activeStyle : undefined
+              }
+              >
+              Shop
+          </NavLink>
+        </li>
+        <li>
+          <button className='cart-button'>Cart</button>
+        
+        </li>
+      </ul>
+    </nav>
+    
   )
 }
 
