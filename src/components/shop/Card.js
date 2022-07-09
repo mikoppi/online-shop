e04f3ba0d30e4831654a707f54b505cdbcd32122
Miko Peltokorpi <miko.peltokorpi@gmail.com>
2022-07-09 19:22:17 +0300

    Styled cards to look like steam store

--- src/components/shop/Card.js
+++ src/components/shop/Card.js
@@ -8,14 +8,24 @@ const Card = ({ name, url, price, normalPrice, saving, review, score }) => {
     
   return (
     <div className='card' id={name} >
-        <img src={url} alt='game' width="250" height="auto"></img>
-        <h3>{name} </h3>
-        <h4 className='normal-price'>{normalPrice}€  </h4>
+        <div className='image'>
+            <img src={url} alt='game' width="250" height="auto"></img>
+            <div class="content">
+                <div class="text">
+                    <a href={metacriticUrl} target='_blank' rel='noreferrer'>Click for review on Metacritic</a>
+                </div>
+            </div>
+        </div>
+        <h3 className='name'>{name} </h3>
         <div className='more'>
-            <h4 className='price'>{price}€  <span>-{Math.floor(saving)}%</span></h4>
-            <h4 className='review'>Review: <span><a href={metacriticUrl} target='_blank' rel='noreferrer'>Metacritic</a></span><span id='score'>{score}</span></h4>
+            <h3>-{Math.floor(saving)}%</h3>
+            <div className='prices'>
+                <h4 className='normal-price'>{normalPrice}€  </h4>
+                <h4 className='price'>{price}€  </h4>
+            </div>
         </div>
         
+        <button className='addcart'>Add to cart</button>
 
     </div>
   )
